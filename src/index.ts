38c4d1cd4efc61874eeee4fb2949c061e002a346
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2025-03-04 00:22:23 +0900

    feat: 404および500エラーハンドリングを追加

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,21 @@ app.use(sessionMiddleware);
 app.use(rootRenderer);
 app.route('/', indexApp);
 
+app
+  .notFound((c) => {
+    c.status(404);
+    return c.render('ページが見つかりませんでした。code: 404', {
+      title: '404 Not Found',
+    });
+  })
+  .onError((err, c) => {
+    console.error(err);
+    c.status(500);
+    return c.render('エラーが発生しました。code: 500', {
+      title: '500 Internal Server Error',
+    });
+  });
+
 serve({
   fetch: app.fetch,
   port: 3000
