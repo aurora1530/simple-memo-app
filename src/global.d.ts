8155446c2b47f177f59026efd4dd90af195d71e7
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2025-03-03 21:19:09 +0900

    feat: JSXレンダラーを使用してヘッダーを追加し、ホームページを更新

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,13 +1,20 @@
 import type {} from 'hono';
 import type { Session } from './sessionMiddleware.ts';
 
-
-
 declare module 'hono' {
   interface Env {
     Variables: {
       session?: Session;
     };
     Bindings: {};
+  }
+
+  interface ContextRenderer {
+    (
+      content: string | Promise<string>,
+      props: {
+        title: string;
+      }
+    ): Response;
   }
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,25 +2,39 @@ import { type Env, Hono } from 'hono';
 import { Layout } from '../layout.js';
 import authApp from './auth/index.js';
 import memoApp from './memo/index.js';
+import { jsxRenderer } from 'hono/jsx-renderer';
+import Header from '../components/common/Header.js';
 
 const indexApp = new Hono<Env>();
+
+indexApp.use(
+  jsxRenderer(({ children, title }) => {
+    return (
+      <html lang="ja">
+        <head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <link rel="stylesheet" href="style.css" />
+          <title>{title}</title>
+        </head>
+        <body>
+          <Header />
+          {children}
+        </body>
+      </html>
+    );
+  })
+);
 
 indexApp.route('/auth', authApp);
 indexApp.route('/memo', memoApp);
 
 indexApp.get('/', (c) => {
-  return c.html(
-    <Layout c={c} title="Home">
-      <h1>Hello World!</h1>
-      <div>
-        <div>
-          <a href="/auth/register">サインアップ</a>
-        </div>
-        <div>
-          <a href="/auth/login">サインイン</a>
-        </div>
-      </div>
-    </Layout>
+  return c.render(
+    <h1>This is Simple Memo App</h1>,
+    {
+      title: 'Home',
+    }
   );
 });
 
