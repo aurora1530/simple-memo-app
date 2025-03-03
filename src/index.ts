2f4690cee751cc1cf3a403163c26d32a465af0e2
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2025-03-03 18:09:44 +0900

    feat: セッションミドルウェア、CSRF保護、ロギング、静的ファイル提供を追加

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import indexApp from './routes/index.js'
+import { sessionMiddleware } from './sessionMiddleware.js'
+import { csrf } from 'hono/csrf'
+import { logger } from 'hono/logger'
+import { serveStatic } from '@hono/node-server/serve-static'
+import { secureHeaders } from 'hono/secure-headers'
 
 const app = new Hono()
 
+app.use(logger());
+app.use(serveStatic({ root: 'public' }));
+app.use(secureHeaders());
+app.use(csrf());
+app.use(sessionMiddleware);
 app.route('/', indexApp);
 
 serve({
