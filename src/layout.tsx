01a76a708737a43885dcd42fd95fca276893a293
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2025-03-03 18:10:00 +0900

    feat: レイアウトにセッション情報を表示するナビゲーションバーを追加

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,10 +1,13 @@
+import type { Context, Env } from 'hono';
 import type { PropsWithChildren } from 'hono/jsx';
 
 interface LayoutProps {
+  c: Context<Env>;
   title: string;
 }
 
-export const Layout = ({ title, children }: PropsWithChildren<LayoutProps>) => {
+export const Layout = ({ c, title, children }: PropsWithChildren<LayoutProps>) => {
+  const session = c.get('session');
   return (
     <html lang="ja">
       <head>
@@ -12,7 +15,26 @@ export const Layout = ({ title, children }: PropsWithChildren<LayoutProps>) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{title}</title>
       </head>
-      <body>{children}</body>
+      <body>
+        <header>
+          <nav>
+            <div><a href="/">Home</a></div>
+            {session?.isLogin ? (
+              <>
+                <p>UserName: {session.username}</p>
+                <a href="/auth/logout">Logout</a>
+              </>
+            ) : (
+              <>
+                <div><a href="/auth/register">Register</a></div>
+                <div><a href="/auth/login">Login</a></div>
+              </>
+            )}
+          </nav>
+        </header>
+
+        {children}
+      </body>
     </html>
   );
 };
