805e3daca7aa41c27e079cf3527cb585c8744d7f
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2025-03-04 19:11:31 +0900

    feat: メモの文字数をグラフェム単位でカウントするフロントJSを追加

--- public/memoForm.js
+++ public/memoForm.js
@@ -6,4 +6,28 @@ document.addEventListener('keydown', (e) => {
   if (e.key === 'Enter' && e.ctrlKey) {
     document.getElementById('memo-form').submit();
   }
-})
+});
+
+/**
+ * cf. https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/String/length
+ */
+const getGraphemeCount = (text) => {
+  const segmenter = new Intl.Segmenter('ja-JP', { granularity: 'grapheme' });
+  return [...segmenter.segment(text)].length;
+};
+
+const memoTitle = document.getElementById('memo-title');
+memoTitle.addEventListener('input', (e) => {
+  const count = getGraphemeCount(e.target.value);
+  document.getElementById('title-char-count').textContent = count;
+});
+
+memoBody.addEventListener('input', (e) => {
+  const count = getGraphemeCount(e.target.value);
+  document.getElementById('body-char-count').textContent = count;
+});
+
+(() => {
+  memoTitle.dispatchEvent(new Event('input'));
+  memoBody.dispatchEvent(new Event('input'));
+})();
