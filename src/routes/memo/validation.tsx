166f297b77c4a334e54287f878bea594cfaf4d70
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2025-03-04 18:58:46 +0900

    feat: メモのタイトルと本文の文字数制限を書記素単位で検証するように変更

--- src/routes/memo/validation.tsx
+++ src/routes/memo/validation.tsx
@@ -1,20 +1,24 @@
 import { zValidator } from '@hono/zod-validator';
 import { z } from 'zod';
 import MemoForm from '../../components/memo/MemoForm.js';
+import { getGraphemeCount } from '../../utils/string.js';
 
-
-export const memoValidation = (type: 'create'|'edit') =>
+export const memoValidation = (type: 'create' | 'edit') =>
   zValidator(
     'form',
     z.object({
       title: z
         .string()
         .min(1, 'タイトルを入力してください')
-        .max(255, 'タイトルは255文字以内で入力してください'),
+        .refine((title) => getGraphemeCount(title) <= 255, {
+          message: 'タイトルは255文字以内で入力してください',
+        }),
       body: z
         .string()
         .min(1, '本文を入力してください')
-        .max(10000, '本文は10000文字以内で入力してください'),
+        .refine((body) => getGraphemeCount(body) <= 10000, {
+          message: '本文は10000文字以内で入力してください',
+        }),
     }),
     (result, c) => {
       if (!result.success) {
