e319bd8717befafaf82728cb241d2ab53fcd606d
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2025-03-04 23:56:29 +0900

    db: migration file

---
+++ prisma/migrations/20250304145505_add_delete_col/migration.sql
@@ -0,0 +1,18 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Memo" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" INTEGER NOT NULL,
+    "title" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "deleted" BOOLEAN NOT NULL DEFAULT false,
+    CONSTRAINT "Memo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Memo" ("body", "createdAt", "id", "title", "updatedAt", "userId") SELECT "body", "createdAt", "id", "title", "updatedAt", "userId" FROM "Memo";
+DROP TABLE "Memo";
+ALTER TABLE "new_Memo" RENAME TO "Memo";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
