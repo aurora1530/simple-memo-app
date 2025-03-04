c092f5f1e819aa9741c4ffa07628dd2622ba0e2a
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2025-03-05 00:07:28 +0900

    feat: sealおよびunseal関数に戻り値の型を追加

--- src/seal.ts
+++ src/seal.ts
@@ -3,12 +3,12 @@ import { env } from 'hono/adapter';
 import * as Iron from 'iron-webcrypto';
 import * as Crypto from 'uncrypto';
 
-export const seal = async (c: Context, data: string) => {
+export const seal = async (c: Context, data: string): Promise<string> => {
   const password = env<{ SESSION_PASSWORD: string }>(c).SESSION_PASSWORD;
   return await Iron.seal(Crypto, data, password, Iron.defaults);
 };
 
-export const unseal = async (c: Context, sealed: string) => {
+export const unseal = async (c: Context, sealed: string): Promise<string> => {
   const password = env<{ SESSION_PASSWORD: string }>(c).SESSION_PASSWORD;
   const unsealed = await Iron.unseal(Crypto, sealed, password, Iron.defaults);
   if (typeof unsealed === 'string') {
